fbfa1b2d90d18507747973752136647e7de177b6
Eluniverso <eluniverso2333@gmail.com>
2022-02-12 15:14:36 +0800

    feat: create delay func add it into main & write sample example into main

--- _02_S32K144_DELAY/Sources/main.c
+++ _02_S32K144_DELAY/Sources/main.c
@@ -15,22 +15,24 @@
 ** @brief
 **         Main module.
 **         This module contains user's application code.
-*/         
+*/
 /*!
 **  @addtogroup main_module main module documentation
 **  @{
-*/         
+*/
 /* MODULE main */
 
-
 /* Including necessary module. Cpu.h contains other modules needed for compiling.*/
-#include "Cpu.h"
+#include "delay.h"
+#include <Cpu.h>
+#include <S32K144.h>
+#include <clockMan1.h>
 
-  volatile int exit_code = 0;
+volatile int exit_code = 0;
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
 
-/*! 
+/*!
   \brief The main function for the project.
   \details The startup initialization sequence is the following:
  * - startup asm routine
@@ -38,31 +40,38 @@
 */
 int main(void)
 {
-  /* Write your local variable definition here */
-
-  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
-  #ifdef PEX_RTOS_INIT
-    PEX_RTOS_INIT();                   /* Initialization of the selected RTOS. Macro is defined by the RTOS component. */
-  #endif
-  /*** End of Processor Expert internal initialization.                    ***/
-
-  /* Write your code here */
-  /* For example: for(;;) { } */
-
-  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
-  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
-  #ifdef PEX_RTOS_START
-    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
-  #endif
-  /*** End of RTOS startup code.  ***/
-  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
-  for(;;) {
-    if(exit_code != 0) {
-      break;
+    /* Write your local variable definition here */
+    int MCUFreq;
+/*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+#ifdef PEX_RTOS_INIT
+    PEX_RTOS_INIT(); /* Initialization of the selected RTOS. Macro is defined by the RTOS component. */
+#endif
+    /*** End of Processor Expert internal initialization.                    ***/
+    CLOCK_SYS_Init(g_clockManConfigsArr, CLOCK_MANAGER_CONFIG_CNT, g_clockManCallbacksArr, CLOCK_MANAGER_CALLBACK_CNT);
+    CLOCK_SYS_UpdateConfiguration(0U, CLOCK_MANAGER_POLICY_AGREEMENT);
+    MCUFreq = Delay_Init();
+    PINS_DRV_Init(NUM_OF_CONFIGURED_PINS, g_pin_mux_InitConfigArr);
+    PINS_DRV_WritePin(PTD, 16, 1);
+    while (1) {
+        PINS_DRV_WritePin(PTD, 15, 0);
+        Delay_ms(1000);
+        PINS_DRV_WritePin(PTD, 15, 1);
+        Delay_ms(1000);
     }
-  }
-  return exit_code;
-  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+/*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+#ifdef PEX_RTOS_START
+    PEX_RTOS_START(); /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+#endif
+    /*** End of RTOS startup code.  ***/
+    /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+    for (;;) {
+        if (exit_code != 0) {
+            break;
+        }
+    }
+    return exit_code;
+    /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
 } /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
 
 /* END main */
